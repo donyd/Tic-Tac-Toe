9e94decf90a6fceb02f522de89574d08e46bb5c3
donyd <donal_dsilva@hotmail.com>
2023-04-05 09:48:28 +0100

    rudimentary play protoyping completed

--- gameLogic.js
+++ gameLogic.js
@@ -47,5 +47,69 @@ window.onload = function() {
       context.strokeStyle = "aliceblue";
       context.stroke();
 
-
 }
+
+function checkMouse(event) {
+    let x = event.offsetX;
+    let y = event.offsetY;
+    document.getElementById("demo_x").innerHTML = "The x-coordinate is: " + x;
+    document.getElementById("demo_y").innerHTML = "The y-coordinate is: " + y;
+    drawCircle(x, y);
+}
+
+
+function drawCircle(xpos, ypos) {
+    var canvas = document.querySelector("canvas");
+    var context = canvas.getContext("2d");
+    let play = false;
+    
+    // Play area bounds check
+    if ( (xpos >= 150 && ypos >= 150) && ( xpos <= 444 && ypos <= 444))
+    {
+        play = true;    
+    } else {
+        play = false;
+    }
+
+    console.log(play);
+
+    if (play)
+    {
+        console.log(play);
+        
+        if ( (xpos < 248 && xpos > 148) ) 
+        {
+            xpos = 198;
+            ypos = 198;
+        }
+    
+        // context.moveTo(xpos, ypos)
+        context.beginPath();
+        context.arc(xpos, ypos, 40, 0, 2 * Math.PI);
+        
+        context.strokeStyle = "black";
+        context.stroke();
+        console.log(xpos + " " + ypos);
+    }
+}
+
+
+/*
+    Marking and representation of the play area:
+        
+    _1_|_2_|_3_
+    _4_|_5_|_6_
+    _7_|_8_|_9_
+
+    Absolute positions (center in x & y coordinates) based on edge of the browser and canvas elements
+    1 - 198, 198
+    2 - 298, 198
+    3 - 398, 198
+    4 - 198, 298
+    5 - 298, 298
+    6 - 398, 298
+    7 - 198, 398
+    8 - 297, 398
+    9 - 398, 398
+
+*/
